ca6b97e909550519ac7bdcca1c9ecdebc0fb84c0
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-07-28 14:08:18 +0000

    Remove C code

--- c/test/replication.h
+++
@@ -1,8 +0,0 @@
-#ifndef DQLITE_TEST_REPLICATION_H
-#define DQLITE_TEST_REPLICATION_H
-
-#include <sqlite3.h>
-
-sqlite3_wal_replication* test_replication();
-
-#endif /* DQLITE_TEST_REPLICATION_H */
