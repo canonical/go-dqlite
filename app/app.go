5822937cdcc6c20aa9d8cb5dacef6d65f8192a09
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-04-02 14:50:05 +0100

    Create and register the local dqlite driver

--- app/app.go
+++ app/app.go
@@ -1,9 +1,20 @@
 package app
 
-import "github.com/canonical/go-dqlite"
+import (
+	"database/sql"
+	"fmt"
+	"path/filepath"
+
+	"github.com/canonical/go-dqlite"
+	"github.com/canonical/go-dqlite/client"
+	"github.com/canonical/go-dqlite/driver"
+)
 
 type App struct {
-	node *dqlite.Node
+	node       *dqlite.Node
+	store      client.NodeStore
+	driver     *driver.Driver
+	driverName string
 }
 
 // New creates a new application node.
@@ -18,13 +29,28 @@ func New(dir string, options ...Option) (*App, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	if err := node.Start(); err != nil {
 		return nil, err
 	}
 
+	store, err := client.DefaultNodeStore(filepath.Join(dir, "servers.sql"))
+	if err != nil {
+		return nil, err
+	}
+
+	driver, err := driver.New(store)
+	if err != nil {
+		return nil, err
+	}
+	driverIndex++
+	driverName := fmt.Sprintf("dqlite-%d", driverIndex)
+	sql.Register(driverName, driver)
+
 	app := &App{
-		node: node,
+		node:       node,
+		store:      store,
+		driver:     driver,
+		driverName: driverName,
 	}
 
 	return app, nil
@@ -37,3 +63,5 @@ func (a *App) Close() error {
 	}
 	return nil
 }
+
+var driverIndex = 0
