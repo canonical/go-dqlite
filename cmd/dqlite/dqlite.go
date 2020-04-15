01ee966677d2781d353e7dabd185fecff8711008
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-04-15 11:47:56 +0100

    Support TLS in dqlite shell

--- cmd/dqlite/dqlite.go
+++ cmd/dqlite/dqlite.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 
+	"github.com/canonical/go-dqlite/app"
 	"github.com/canonical/go-dqlite/client"
 	"github.com/canonical/go-dqlite/internal/shell"
 	"github.com/peterh/liner"
@@ -13,20 +17,51 @@ import (
 )
 
 func main() {
-	var cluster *[]string
+	var crt string
+	var key string
+	var servers *[]string
 
 	cmd := &cobra.Command{
-		Use:   "dqlite -c <cluster> <database>",
+		Use:   "dqlite -s <servers> <database>",
 		Short: "Standard dqlite shell",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			servers := make([]client.NodeInfo, len(*cluster))
-			for i, address := range *cluster {
-				servers[i].Address = address
+			infos := make([]client.NodeInfo, len(*servers))
+			for i, address := range *servers {
+				infos[i].Address = address
 			}
+
 			store := client.NewInmemNodeStore()
-			store.Set(context.Background(), servers)
-			sh, err := shell.New(args[0], store)
+			store.Set(context.Background(), infos)
+
+			if (crt != "" && key == "") || (key != "" && crt == "") {
+				return fmt.Errorf("both TLS certificate and key must be given")
+			}
+
+			dial := client.DefaultDialFunc
+
+			if crt != "" {
+				cert, err := tls.LoadX509KeyPair(crt, key)
+				if err != nil {
+					return err
+				}
+
+				data, err := ioutil.ReadFile(crt)
+				if err != nil {
+					return err
+				}
+
+				pool := x509.NewCertPool()
+				if !pool.AppendCertsFromPEM(data) {
+					return fmt.Errorf("bad certificate")
+				}
+
+				config := app.SimpleDialTLSConfig(cert, pool)
+				dial = client.DialFuncWithTLS(dial, config)
+
+			}
+
+			sh, err := shell.New(args[0], store, shell.WithDialFunc(dial))
 			if err != nil {
 				return err
 			}
@@ -56,9 +91,11 @@ func main() {
 	}
 
 	flags := cmd.Flags()
-	cluster = flags.StringSliceP("cluster", "c", nil, "comma-separted list of db nodes")
+	servers = flags.StringSliceP("servers", "s", nil, "comma-separated list of db servers")
+	flags.StringVarP(&crt, "cert", "c", "", "public TLS cert")
+	flags.StringVarP(&key, "key", "k", "", "private TLS key")
 
-	cmd.MarkFlagRequired("cluster")
+	cmd.MarkFlagRequired("servers")
 
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
