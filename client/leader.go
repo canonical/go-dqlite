214de755aa78da1aa69d67078553741b16b9e076
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-06-04 08:59:59 +0100

    Improve FindLeader docstring

--- client/leader.go
+++ client/leader.go
@@ -6,7 +6,12 @@ import (
 	"github.com/canonical/go-dqlite/internal/protocol"
 )
 
-// FindLeader returns a Client connected to the current cluster leader, if any.
+// FindLeader returns a Client connected to the current cluster leader.
+//
+// The function will iterate through to all nodes in the given store, and for
+// each of them check if it's the current leader. If no leader is found, the
+// function will keep retrying (with a capped exponential backoff) until the
+// given context is canceled.
 func FindLeader(ctx context.Context, store NodeStore, options ...Option) (*Client, error) {
 	o := defaultOptions()
 
