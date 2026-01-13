246afca3840cf5b9d7037fe32436502efebcff14
Kadin Sayani <kadin.sayani@canonical.com>
2026-01-13 12:23:23 -0800

    client: Add RoleMask type
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- client/constants.go
+++ client/constants.go
@@ -10,3 +10,28 @@ const (
 	StandBy = protocol.StandBy
 	Spare   = protocol.Spare
 )
+
+// RoleMask identifies which roles a node is allowed to hold.
+type RoleMask uint8
+
+const (
+	RoleMaskVoter RoleMask = 1 << iota
+	RoleMaskStandBy
+	RoleMaskSpare
+
+	RoleMaskAll = RoleMaskVoter | RoleMaskStandBy | RoleMaskSpare
+)
+
+// RoleMaskFor returns the mask bit for the given role.
+func RoleMaskFor(role NodeRole) RoleMask {
+	switch role {
+	case Voter:
+		return RoleMaskVoter
+	case StandBy:
+		return RoleMaskStandBy
+	case Spare:
+		return RoleMaskSpare
+	default:
+		return 0
+	}
+}
